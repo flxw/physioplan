91e97911cc67c8b4b5c08a5b3bc8dbf21f1bb436
Felix Wolff <felix.wolff2@student.hpi.de>
2020-01-26 11:25:48 +0100

    Refactoring

---
+++ src/app/plan-master/plan-master.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-master',
+  templateUrl: './plan-master.component.html',
+  styleUrls: ['./plan-master.component.styl']
+})
+export class PlanMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/plan-selector/plan-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-selector',
+  templateUrl: './plan-selector.component.html',
+  styleUrls: ['./plan-selector.component.styl']
+})
+export class PlanSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
